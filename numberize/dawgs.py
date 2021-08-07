24f53c023742635287038cc358da069de4c33740
DanATW <emchenko@dlit.dp.ua>
2021-08-07 18:20:12 +0300

    feat(numberize.dawgs): add dawgs for langs

---
+++ numberize/dawgs.py
@@ -0,0 +1,7 @@
+import dawg
+
+import dicts
+
+ru_nums = dawg.IntDAWG(dicts.ru.ru_nums)
+uk_nums = dawg.IntDAWG(dicts.uk.uk_nums)
+en_nums = dawg.IntDAWG(dicts.en.en_nums)
---
+++ numberize/dicts/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from . import en
+from . import ru
+from . import uk
