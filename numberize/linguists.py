03c53e288fb0399bc6f628d731c1106879718ebc
DanATW <emchenko@dlit.dp.ua>
2021-08-09 00:12:24 +0300

    refactor(numberize.linguists): optimize if else

--- numberize/linguists.py
+++ numberize/linguists.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 
-import dawgs
+import numberize.dawgs as dawgs
 
 
 class Linguist(ABC):
+    @staticmethod
     @abstractmethod
-    def get_number(self, token: str):
+    def get_number(token: str):
         """
         E.g. 'two' -> 2, 'twenty-five' -> 25, "п'ять" -> 5, "миллион" -> 1E6
         'horse' -> None
@@ -15,14 +16,19 @@ class Linguist(ABC):
 
 
 class EnLinguist(Linguist):
-    def get_number(self, token: str):
+    @staticmethod
+    def get_number(token: str):
         if '-' in token:
             parts = token.split('-')
             if len(parts) != 2:
                 return
             left = dawgs.en_nums.get(parts[0])
+            if not left or left < 20 or left > 90:
+                return
             right = dawgs.en_nums.get(parts[1])
-            return left + right if left and right else None
+            if not right or right > 9 or right < 1:
+                return
+            return left + right
         return dawgs.en_nums.get(token)
 
 
