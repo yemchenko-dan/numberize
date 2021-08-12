5a8a9543a41273ac4921473dd556516a0626ab07
DanATW <emchenko@dlit.dp.ua>
2021-08-12 15:47:44 +0300

    test(tests.test_linguists): refactor

--- tests/test_linguists.py
+++ tests/test_linguists.py
@@ -1,14 +1,16 @@
+import pytest
+
 import numberize.linguists as linguists
 
-
-def test_en_linguist():
-    test_data = [
+EN_DATA = [
         ('twenty-five', 25), ('one', 1), ('hundred', 100),
         ('million', 1000000), ('billion', 1000000000), ('ruler', None),
         ('twenty-ten', None), ('one-two', None), ('two-three-four', None),
         ('yeay-sheeh', None), ('пять', None)
-    ]
+]
+
+
+@pytest.mark.parametrize("token,expected_output", EN_DATA)
+def test_en_linguist(token, expected_output):
     ling = linguists.EnLinguist()
-    for q_a in test_data:
-        q, a = q_a
-        assert a == ling.get_number(q)
+    assert ling.get_number(token) == expected_output
